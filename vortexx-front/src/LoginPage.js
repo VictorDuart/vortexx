fd578e641a931b51e928f0bb7baf5df273ae1064
VictorDuart <victorhd2014@gmail.com>
2024-09-07 20:24:03 -0300

    primeiro commit

---
+++ vortexx-front/src/LoginPage.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aqui você pode fazer a autenticação do usuário.
+    console.log(`Username: ${username}, Password: ${password}`);
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={handleUsernameChange} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={handlePasswordChange} />
+        </label>
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
---
+++ vortexx/src/test/java/com/vhdf/vortexx/VortexxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vhdf.vortexx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VortexxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
